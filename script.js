b104f28a4bf813a1e649207c8f375a58c05f29ad
Lucas Vinn <90799507+LucasVinn@users.noreply.github.com>
2022-08-29 15:20:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// ScrollReveal Code
+
+/* --- NAV --- */
+ScrollReveal().reveal('.nav', {delay: 500});
+/* --- MAIN --- */
+ScrollReveal().reveal('.section-1', {delay: 650});
+ScrollReveal().reveal('.section-2', {delay: 670});
+ScrollReveal().reveal('.section-3', {delay: 690});
+
+// General Javascript
